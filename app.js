00508066baaba4ea60042a95b8200d85677fd992
Nguyenminhthuc12032001 <nguyenminhthuc12032001@gmail.com>
2025-09-10 16:10:31 +0700

    Thuc update code global

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const adminRoutes = require('./routes/user.routes');
+const userRoutes = require('./routes/user.routes');
 const petRoutes = require('./routes/pet.routes');
 app.use(cors());
 app.use(express.json());
