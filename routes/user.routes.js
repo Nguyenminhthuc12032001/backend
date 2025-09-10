7d6aaf5fd0ed83b90e552bcfbe733622dd1f8c40
Nguyenminhthuc12032001 <nguyenminhthuc12032001@gmail.com>
2025-09-10 18:31:05 +0700

    Thuc update code global

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,12 +3,12 @@ const { verifyToken, checkRole } = require('../middlewares/authentication.middle
 const adminController = require('../controllers/user.controller');
 const { validUser, validUserUpdate } = require('../middlewares/validUser.middlewares');
 
-router.post('/createNew', [/*verifyToken, checkRole('admin')*/, validUser], adminController.createNew);
+router.post('/createNew', [verifyToken, checkRole('admin'), validUser], adminController.createNew);
 router.get('/getAll', [verifyToken, checkRole('admin')], adminController.getAll);
 router.get('/get/:id', [verifyToken, checkRole('admin')], adminController.get);
 router.put('/update/:id', [verifyToken, checkRole('admin'), validUserUpdate], adminController.update);
-router.post('/remove/:id', [verifyToken, checkRole('superAdmin')], adminController.remove);
-router.get('/search', [verifyToken, checkRole('superAdmin')], adminController.search);
-router.post('/login', [validUser], adminController.login);
+router.post('/remove/:id', [verifyToken, checkRole('admin')], adminController.remove);
+router.get('/search', [verifyToken, checkRole('admin')], adminController.search);
+router.post('/login', [validUserUpdate], adminController.login);
 
 module.exports = router;
